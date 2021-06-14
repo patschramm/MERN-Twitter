d779e253663b8ea2f4583023b6fef1a98d183d09
patschramm <>
2021-06-13 18:37:34 -0700

    did ALL the work

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User");
+const bodyParser = require("body-parser");
+
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
+    const user = new User({
+        handle: "pat",
+        email: "pat@pat.pat",
+        password: "patismeh123"
+    })
+    user.save();
     res.send("Hello World!");
 });
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
